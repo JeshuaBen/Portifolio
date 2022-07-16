9b02b3f0d507d3fbe1f0052cb4c5e0f2391189b5
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-16 12:39:24 -0300

    feat: added the chakraUI to use template modals

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Main } from "./pages/Main";
 import { ThemeProvider } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import theme from "./global/theme";
+import { Main } from "./pages/Main";
 
 import GlobalStyles from "./global/global";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Main />
+      <ChakraProvider>
+        <GlobalStyles />
+        <Main />
+      </ChakraProvider>
     </ThemeProvider>
   );
 }
